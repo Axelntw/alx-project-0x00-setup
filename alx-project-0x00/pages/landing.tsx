228f52e7048fc0007beb9c86f2e4b6fb33491351
Axelntw <ntwaliaxel@gmail.com>
2025-06-01 10:29:53 +0200

    Task 6

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,9 +1,17 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button"
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
+      
+      <div className="flex gap-4 p-4">
+        <Button title="Small" size="small" shape="rounded-sm" />
+        <Button title="Medium" size="medium" shape="rounded-md" />
+        <Button title="Large" size="large" shape="rounded-full" />
+      </div>
+
       <div className="flex flex-wrap gap-4 p-4">
         <Card />
         <Card />
