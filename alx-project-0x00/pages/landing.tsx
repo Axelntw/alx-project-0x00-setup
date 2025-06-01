242338602009410358bec2633c16fd7a19cad10b
Axelntw <ntwaliaxel@gmail.com>
2025-06-01 10:38:56 +0200

    Task 6 Fixes

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -7,9 +7,10 @@ const Landing: React.FC = () => {
       <h1 className="text-xl font-extralight">Landing Page</h1>
       
       <div className="flex gap-4 p-4">
-        <Button title="Small" size="small" shape="rounded-sm" />
-        <Button title="Medium" size="medium" shape="rounded-md" />
-        <Button title="Large" size="large" shape="rounded-full" />
+        <Button title="Small" size="small" shape="rounded-sm" styles="bg-blue-500" />
+        <Button title="Medium" size="medium" shape="rounded-md" styles="bg-green-500" />
+        <Button title="Large" size="large" shape="rounded-lg" styles="bg-red-500" />
+        <Button title="Full" size="medium" shape="rounded-full" styles="bg-purple-500" />
       </div>
 
       <div className="flex flex-wrap gap-4 p-4">
